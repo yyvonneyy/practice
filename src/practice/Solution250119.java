8ea5665418874b369c986dedfa4fd16d8284a3d0
Minjeong <my949437@naver.com>
2025-01-19 22:32:08 +0900

    25.01.19 정수 부분 구하기

---
+++ src/practice/Solution250119.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Solution250119 {
+	public static void main(String[] args) {
+		//정수 부분 구하기
+		int answer = 0;
+		double flo = 69.32;
+
+		answer = (int) Math.floor(flo);
+		System.out.println(answer);
+		
+		//Math.floor() ⇒　내림
+		//Math.ceil() ⇒ 올림
+		//Math.round() ⇒ 반올림
+	}
+}
